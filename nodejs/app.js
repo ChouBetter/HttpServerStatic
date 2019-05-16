a326dee8193bc59a0952f2d09e5a96219da640e5
Better <>
2019-05-16 23:36:23 +0800

    cancel cache

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,6 +3,10 @@ var app = express();
 app.get("/", function(req, res) {
   res.redirect("/index.html");
 });
-app.use(express.static(__dirname + "/www"));
+app.use(
+  express.static(__dirname + "/www", {
+    etag: false
+  })
+);
 app.listen(3000);
 console.log("Listening on port 3000");
